c7a839fa893146addc1ec8c9ec5b72f65d2783fc
Serpahus <kenko04@yandex.ru>
2021-06-16 00:09:15 +0300

    tcpserver
    server

---
+++ server/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using NLog;
+
+
+namespace server
+{
+    public class ClientObject
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public TcpClient client;
+
+        public ClientObject(TcpClient tcpClient)
+        {
+            client = tcpClient;
+
+        }
+
+        public void Process()
+        {
+
+            NetworkStream stream = null;
+            try
+            {
+
+                stream = client.GetStream();
+
+
+                byte[] data = new byte[256]; // буфер для получаемых данных
+                while (true)
+                {
+
+                    // получаем сообщение
+                    StringBuilder builder = new StringBuilder();
+
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+
+                        builder.Append(Encoding.ASCII.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+                 
+
+                    string message = builder.ToString();
+                    logger.Trace("получение : " + message);
+
+                    Console.WriteLine(message);
+                    // отправляем обратно сообщение сумму кодов ascii
+
+                    int result = 0;
+                    for (int i = 0; i < message.Length; ++i)
+                    {
+                        result += message[i];
+                    }
+                    message = Convert.ToString(result);
+                    data = Encoding.ASCII.GetBytes(message);
+                    stream.Write(data, 0, data.Length);
+                    logger.Trace("отправка : " + message);
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+                if (client != null)
+                    client.Close();
+            }
+        }
+        class Program
+        {
+            const int port = 8888;
+            static TcpListener listener;
+            static void Main(string[] args)
+            {
+                try
+                {
+                    listener = new TcpListener(System.Net.IPAddress.Parse("127.0.0.1"), port);
+
+                    listener.Start();
+                    
+
+
+                    Console.WriteLine("Ожидание подключений...");
+
+                    while (true)
+                    {
+
+                        TcpClient client = listener.AcceptTcpClient();
+
+                        ClientObject clientObject = new ClientObject(client);
+
+
+                        // создаем новый поток для обслуживания нового клиента
+                        Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
+                        clientThread.Start();
+
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    if (listener != null)
+                        listener.Stop();
+                }
+            }
+        }
+    }
+}
---
+++ tcpconsole/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace tcpconsole
+{
+    class Program
+    {
+        const int port = 8888;
+        const string address = "127.0.0.1";
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свое имя:");
+            string userName = Console.ReadLine();
+            TcpClient client = null;
+            try
+            {
+                client = new TcpClient(address, port);
+                NetworkStream stream = client.GetStream();
+
+                while (true)
+                {
+                    Console.Write(userName + ": ");
+                    Console.Write("Колличество повторений до 100: ");
+                    int repeat = int.Parse(Console.ReadLine());
+                    // ввод сообщения
+                    Console.Write("Введите сообщение до 20 символов: ");
+                    string message = Console.ReadLine();
+                    // преобразуем сообщение в массив байтов
+                    byte[] data = Encoding.ASCII.GetBytes(message);
+                    // отправка сообщения
+                    for (int count = 0; count < repeat; count++)
+                    {
+                        stream.Write(data, 0, data.Length);
+                    }
+                        
+                    // получаем ответ
+                    data = new byte[256]; // буфер для получаемых данных
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.ASCII.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    message = builder.ToString();
+                    Console.WriteLine("Сервер: {0}", message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+    }
+}
